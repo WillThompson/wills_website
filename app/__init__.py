fbda66f40b0c1e5597eb93ead6e43d6ece14d7c9
Will Thompson <will.thompson@zoho.com>
2018-01-01 08:26:26 -1000

    Small change to init

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Sub
 DEBUG=True
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
  
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
