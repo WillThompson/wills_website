8acf5e21e5e307e42d3de4d77b70617c1984bcf2
Will Thompson <will.thompson@zoho.com>
2018-01-03 21:42:25 -0800

    Trying some stuff with input. Pushing latest changes for fun.

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,35 @@
+import os
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from subprocess import call
 
 DEBUG=True
 app = Flask(__name__)
 app.config.from_object(__name__)
- 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 @app.route('/')
 def home():
   return render_template('home.html')
 
+class ReusableForm(Form):
+	name = TextField('Name:', validators=[validators.required()])
+
 @app.route('/form/',methods=['GET', 'POST'])
-def test():
+def enter():
+
 	form = ReusableForm(request.form)
-	print form.errors
+	print(form.errors)
+
 	if request.method == 'POST':
-		name=request.form['name']
-		print name
- 
-        if form.validate():
-            # Save the comment here.
-            flash('Hello ' + name)
-        else:
-            flash('All the form fields are required. ')
+		usr = request.form['usr']
+		name = request.form['name']
+		typ = request.form['typ']
+
+		print(usr + name + typ)
+		print(os.getcwd())
+		call("echo "+ usr + " -- " + name + " -- " + typ +" > filename.txt" , shell=True) 
+
  	return render_template('form.html',form=form)
 
 if __name__ == "__main__":
