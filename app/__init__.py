33fc20993a5f088b69bc60127854c3c021768079
Will Thompson <will.thompson@zoho.com>
2018-01-13 12:47:43 -0800

    Making some modifications to the website to cleanup appearance and testing input to new pages.

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,20 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
-		t1 = request.form['test1']
-		t2 = request.form['test2']
-		return render_template('home.html', t1=t1, t2=t2)
+		data1 = request.form['test1']
+		data2 = request.form['test2']
+		return redirect(url_for('help', input1=data1, input2=data2))
 	return render_template('home.html')
 
 class ReusableForm(Form):
 	name = TextField('Name:', validators=[validators.required()])
+
+@app.route('/help?input1=<input1>&input2=<input2>',methods=['GET', 'POST'])
+def help(input1,input2):
+ 	return render_template('dir_list.html',input1=input1,input2=input2)
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
 
 
 @app.route('/dir_list/',methods=['GET', 'POST'])
