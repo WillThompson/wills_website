a3a12db163c7f5a9c40b413fbab2398003264941
Will Thompson <will.thompson@zoho.com>
2018-01-30 22:52:46 -0800

    Creating frequency test functionality

--- app/__init__.py
+++ app/__init__.py
@@ -9,21 +9,22 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
+
 ## MAIN PAGE
 @app.route('/')
 def home():
 	return render_template('home.html')
 
 ## ABOUT ME
-@app.route('/aboutme', methods=['GET', 'POST'])
+@app.route('/aboutme')
 def about():
 	return render_template('aboutme.html')
 
 @app.route('/compute', methods=['GET', 'POST'])
 def compute():
 	if request.method == 'POST':
-		data1 = request.form['test1']
-		return redirect(url_for('output', data=data1))
+		dat = request.form['rand']
+		return redirect(url_for('output', data=dat))
 	return render_template('compute.html')
 
 
@@ -32,38 +33,19 @@ import base64
 #import matplotlib.pyplot as plt
 @app.route('/response?data=<data>', methods=['GET', 'POST'])
 def output(data):
-	data = [float(x) for x in data.split(",")]
+	data = [int(x) for x in data.split(",")]
+	op = performFrequencyTest(data)
 
-	#img = StringIO.StringIO()
+ 	return render_template('output.html',pval = op)
 
-	#data = np.random.normal(0,1,100)
-	#plt.clf()
-	#makeplot(data)
-
-	#plt.savefig(img, format='png')
-	#img.seek(0)
-
-	#plot_url = base64.b64encode(img.getvalue())
-
-	op = np.mean(data)
-
- 	return render_template('output.html',plot_url = data)
-
-
-def makeplot(data):
-	n, bins, patches = plt.hist(data, 50, normed=1, facecolor='g', alpha=0.75)
-	plt.xlabel('x')
-	plt.ylabel('y')
-	plt.title('Plot Title')
-	plt.grid(True)
-
-# @app.route('/dir_list/',methods=['GET', 'POST'])
-# def list_dir():
-
-# 	from os import listdir
-# 	from os.path import isfile, join
-# 	onlyfiles = [f for f in listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
-#  	return render_template('dir_list.html',var1=onlyfiles)
+from scipy.stats import chisquare
+def performFrequencyTest(data):
+	N = len(data)
+	m,M = min(data),max(data)
+	counts = [data.count(x) for x in range(m,M+1)]
+	expected = [float(N)/(M - m) for x in range(m,M+1)]
+	p = chisquare(counts, expected)[1]
+	return(p)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
